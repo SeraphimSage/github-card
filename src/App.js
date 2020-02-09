719736a604c20cff7ab3c96c643f96d9d6fe58c5
SeraphimSage <kenneth.pinkerton90@gmail.com>
2020-02-08 18:32:50 -0700

    Get Data from the Github API and Update State

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const gitHubUserInfoURL = gitHubUserName =>
+	`https://api.github.com/users/${gitHubUserName}`;
 class App extends Component {
-	handleToggle(event) {
-		console.log("hello from the button side");
-	}
+	state = {
+		user: {},
+		active: false
+	};
+	handleToggle = event => {
+		fetch(gitHubUserInfoURL("SeraphimSage"))
+			.then(response => response.json())
+			.then(responsBody => {
+				console.log(responsBody);
+				this.setState({ user: responsBody });
+			});
+	};
 	render() {
 		return <button onClick={this.handleToggle}>Toggle User</button>;
 	}
