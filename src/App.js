d12675a3e566a954249a714bd2cffefc1e89769b
SeraphimSage <kenneth.pinkerton90@gmail.com>
2020-02-08 18:50:37 -0700

    Render Github User Info on the Page

--- src/App.js
+++ src/App.js
@@ -9,15 +9,31 @@ class App extends Component {
 		active: false
 	};
 	handleToggle = event => {
-		fetch(gitHubUserInfoURL("SeraphimSage"))
-			.then(response => response.json())
-			.then(responsBody => {
-				console.log(responsBody);
-				this.setState({ user: responsBody });
-			});
+		if (this.state.active === false) {
+			fetch(gitHubUserInfoURL("SeraphimSage"))
+				.then(response => response.json())
+				.then(responsBody => {
+					console.log(responsBody);
+					this.setState({ user: responsBody, active: true });
+				});
+		} else {
+			this.setState({ active: false });
+		}
 	};
 	render() {
-		return <button onClick={this.handleToggle}>Toggle User</button>;
+		return (
+			<>
+				<button onClick={this.handleToggle}>Toggle User</button>
+				{this.state.active && (
+					<>
+						<img src={this.state.user.avatar_url} alt="user profile" />
+						<h1>{this.state.user.name}</h1>
+						<p>Followers: {this.state.user.followers}</p>
+						<p>Following: {this.state.user.following}</p>
+					</>
+				)}
+			</>
+		);
 	}
 }
 
